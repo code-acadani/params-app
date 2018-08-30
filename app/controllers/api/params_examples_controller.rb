f3b608c585a214fc0b2b4b73fce34269d5bcee99
dani <dani.zaghian@gmail.com>
2018-08-30 11:05:21 -0700

    adds url segment parameters

--- app/controllers/api/params_examples_controller.rb
+++ app/controllers/api/params_examples_controller.rb
@@ -7,4 +7,62 @@ class Api::ParamsExamplesController < ApplicationController
 		render 'query_params.json.jbuilder'
 	end
 
+	def name_method
+		@name = params["name"].upcase
+		@message = "Hey your name doesn't start with A!"
+		# if your name starts with A, display message
+		if @name.starts_with?("A")
+			@message = "Hey your name starts with A!"
+		end
+		render 'name.json.jbuilder'
+	end
+
+	def guess_game_method
+		number = 12
+		user_guess = params["user_guess"].to_i
+		if user_guess > number
+			@message = "Guess lower!"
+		elsif user_guess < number
+			@message = "Guess higher!"
+		else
+			@message = "You win!"
+		end
+		render 'guess_game.json.jbuilder'
+	end
+
+	def segment_params_method
+		input_value = params["tacocat"]
+		@message = "The url segment is #{input_value}"
+		render 'segment_params.json.jbuilder'
+	end
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   # verb "/urls" => "namespace/controllers#action"
 
   namespace :api do
+  	# query params
 		get "/query_params_url" => "params_examples#query_params_method"
+		get "/name_url" => "params_examples#name_method"
+		get "/guess_game_url" => "params_examples#guess_game_method"
+
+		# url segment params
+		get "/segment_params_url/:tacocat" => "params_examples#segment_params_method"
   end
 end
